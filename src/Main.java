128620536b032e8e781af9257f93a1b95d91f713
GustavoLyra23 <gustavolyra23@gmail.com>
2024-06-08 19:15:40 -0300

    Department entities class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
 
 
     }
---
+++ src/model/entities/Department.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + id;
+    }
+}
